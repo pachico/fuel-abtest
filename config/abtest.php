11738a9f35761b049fad6c830c7914414f3f499d
pachico <pachico@gmail.com>
2012-12-24 17:01:52 +0100

    Config values documented

--- config/abtest.php
+++ config/abtest.php
@@ -13,19 +13,19 @@
  */
 return array(
 	/**
-	 * 
+	 * Name the session key has to store the test
 	 */
 	'session_name' => 'abtests',
 	/**
-	 * 
+	 * Name of the cookie
 	 */
 	'cookie_name' => 'abtests',
 	/**
-	 * 
+	 * Time-to-live of the cookie
 	 */
 	'cookie_ttl' => 86400,
 	/**
-	 * 
+	 * Tests to perform
 	 */
 	'tests' => array(
 		'greeting' => array(
